38e9256761ee82c70fcf52dc83d47f0f7337bf31
tfmIV <45746204+tfmIV@users.noreply.github.com>
2019-02-23 20:33:40 -0500

    Update TeamCommitted2DistanceSensorsSimplified.ino

--- TeamCommitted2DistanceSensorsSimplified/TeamCommitted2DistanceSensorsSimplified.ino
+++ TeamCommitted2DistanceSensorsSimplified/TeamCommitted2DistanceSensorsSimplified.ino
@@ -1,54 +1,22 @@
-#include <Encoder.h>
-
-
-// motor stuff
-const int m1_1 = 8;
-const int m1_2 = 7;
-const int m2_1 = 11;
-const int m2_2 = 12;
-const int m1_pwm = 9;
-const int m2_pwm = 10;
-const int ledPin = 13;
 
 // distance sensor CE pins
 const int cePin1 = 0;
-const int cePin2 = 1;
+const int cePin2 = 2;
 
-bool fullSpeedAhead = true;
-
- long original1 = 0;
- long original2 = 0;
-
-// include libraries
-#include <Encoder.h>
-#include <Wire.h>
+// include sensor library
 #include <VL6180X.h>
+#include <Wire.h>
 
 // create sensors 1 and 2
 VL6180X sensor1;
 VL6180X sensor2;
 
-// create encoder knobLeft (pins 2 and 4)
-// create encoder knobRight (pins 3 and 5)
-Encoder knobLeft(2, 4);
-Encoder knobRight(3, 5);
-
 void setup() {
 
-  // set motor pins and CE pins to outputs
-  pinMode(m1_1, OUTPUT);
-  pinMode(m1_2, OUTPUT);
-  pinMode(m1_pwm, OUTPUT);
-  pinMode(m2_1, OUTPUT);
-  pinMode(m2_1, OUTPUT);
-  pinMode(m2_pwm, OUTPUT);
-  pinMode(cePin1, OUTPUT);
-  pinMode(cePin2, OUTPUT);
-
- // begin serial data transmission at 9600 bits/sec (this rate is called baud)
+  // begin serial data transmission at 9600 bits/sec (this rate is called baud)
   Serial.begin(9600);
-
-  // begin wire library
+  
+  // begin wire??
   Wire.begin();
 
   // turn CE pins of both sensors off
@@ -72,6 +40,8 @@ void setup() {
   sensor1.stopContinuous();
   delay(500);
 
+  // turn CE pin 1 off
+  digitalWrite(cePin1, LOW);
   // turn CE pin 2 on
   digitalWrite(cePin2, HIGH);
   delay(50);
@@ -87,6 +57,9 @@ void setup() {
   // stop sensor 2 for now
   sensor2.stopContinuous();
   delay(500);
+
+  // turn CE pin 1 back on
+  digitalWrite(cePin1, HIGH);
 
 }
 
@@ -104,6 +77,7 @@ void loop() {
   Serial.print(sensor1Val);
   Serial.print("Sensor 2: ");
   Serial.print(sensor2Val);
-  delay(1000);
+  Serial.println();
+  delay(100);
 
 }
