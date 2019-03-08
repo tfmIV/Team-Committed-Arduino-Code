6434880f2dd9db795d43ef92de3d85438050fa4b
tfmIV <45746204+tfmIV@users.noreply.github.com>
2019-03-07 22:42:33 -0500

    Updated Code

--- TeamCommittedMasterCode/motors.h
+++ TeamCommittedMasterCode/motors.h
@@ -20,5 +20,76 @@ void setupMotors() {
 	pinMode(m2_pwm, OUTPUT);
 }
 
+//Go forward motor 1
+void forward1(int spd) {
+  digitalWrite(m1_1, LOW);
+  digitalWrite(m1_2, HIGH);
+  analogWrite(m1_pwm, spd);
+}
+
+//Go forward motor 2
+void forward2(int spd) {
+  digitalWrite(m2_1, LOW);
+  digitalWrite(m2_2, HIGH);
+  analogWrite(m2_pwm, spd);
+}
+
+//Go backward motor 1
+void backward1(int spd) {
+  digitalWrite(m1_1, HIGH);
+  digitalWrite(m1_2, LOW);
+  analogWrite(m1_pwm, spd);
+}
+
+//Go backward motor 2
+void backward2(int spd){
+  digitalWrite(m2_1, HIGH);
+  digitalWrite(m2_2, LOW);
+  analogWrite(m2_pwm, spd);
+}
+
+void go(int spd, int mot){
+  
+  if (mot == 1) {
+   if (spd > 0){
+    forward1(spd);
+  }
+   else if (spd < 0){
+    backward1(-spd);
+  }
+   else {
+    digitalWrite(m1_1, LOW);
+    digitalWrite(m1_2, LOW);
+    analogWrite(m1_pwm, 0);
+  }
+  }
+  
+  if (mot == 2) {
+   if (spd > 0){
+    forward2(spd);
+  }
+   else if (spd < 0){
+    backward2(-spd);
+  }
+   else {
+    digitalWrite(m2_1, LOW);
+    digitalWrite(m2_2, LOW);
+    analogWrite(m2_pwm, 0);
+  }
+  }
+}
+
+void turnLeft(int time, int spd) {
+	go(spd, 2);
+	go(spd, 1);
+	delay(time);
+}
+
+void turnRight(int time, int spd) {
+	go(-spd, 1);
+	go(-spd, 2);
+	delay(time);
+}
+
 
 #endif
--- TeamCommittedMasterCode/navigation.h
+++ TeamCommittedMasterCode/navigation.h
@@ -1,11 +1,124 @@
 #ifndef NAVIGATION_FILE
 #define NAVIGATION_FILE
 
-int startDir = 0;
 int north = 0;
 int south = 2;
 int east = 1;
 int west = 3;
+int facing = 0;
 
+bool wallBehind = true;
+
+bool isWallNorth() {
+	if (facing == 0) {
+		return isWallFront();
+	} else if (facing == 1) {
+		return isWallLeft();
+	} else if (facing == 2) {
+		return wallBehind;
+	} else {
+		return isWallRight();
+	}
+}
+
+bool isWallSouth() {
+	if (facing == 0) {
+		return wallBehind;
+	} else if (facing == 1) {
+		return isWallRight();
+	} else if (facing == 2) {
+		return isWallFront();
+	} else {
+		return isWallLeft();
+	}
+}
+
+bool isWallEast() {
+	if (facing == 0) {
+		return isWallRight();
+	} else if (facing == 1) {
+		return isWallFront();
+	} else if (facing == 2) {
+		return isWallLeft();
+	} else {
+		return wallBehind;
+	}
+}
+
+bool isWallWest() {
+	if (facing == 0) {
+		return isWallLeft();
+	} else if (facing == 1) {
+		return wallBehind;
+	} else if (facing == 2) {
+		return isWallRight();
+	} else {
+		return isWallFront();
+	}
+}
+
+void moveNorth() {
+	if (facing == 0) {
+		goOneSquare();
+	} else if (facing == 1) {
+		turnLeft90();
+		goOneSquare();
+	} else if (facing == 2) {
+		turnLeft90();
+		turnLeft90();
+		goOneSquare();
+	} else {
+		turnRight90();
+		goOneSquare();
+	}
+}
+
+void moveSouth() {
+	if (facing == 0) {
+		turnLeft90();
+		turnLeft90();
+		goOneSquare();
+	} else if (facing == 1) {
+		turnRight90();
+		goOneSquare();
+	} else if (facing == 2) {
+		goOneSquare();
+	} else {
+		turnLeft90();
+		goOneSquare();
+	}
+}
+
+void moveEast() {
+	if (facing == 0) {
+		turnRight90();
+		goOneSquare();
+	} else if (facing == 1) {
+		goOneSquare();
+	} else if (facing == 2) {
+		turnLeft90();
+		goOneSquare();
+	} else {
+		turnLeft90();
+		turnLeft90();
+		goOneSquare();
+	}
+}
+
+void moveWest() {
+	if (facing == 0) {
+		turnLeft90();
+		goOneSquare();
+	} else if (facing == 1) {
+		turnLeft90();
+		turnLeft90();
+		goOneSquare();
+	} else if (facing == 2) {
+		turnRight90();
+		goOneSquare();
+	} else {
+		goOneSquare();
+	}
+}
 
 #endif
